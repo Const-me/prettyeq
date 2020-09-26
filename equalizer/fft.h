f624d0285a1ffe9d79a3410c63d1a85d4ca31199
Konstantin <const@const.me>
2020-09-26 17:40:51 +0200

    Minor

--- equalizer/fft.h
+++ equalizer/fft.h
@@ -12,13 +12,15 @@
 #define FFT_SAMPLE_TO_FREQ(NUM_SAMPLES, SAMPLE_INDEX) (44100*(SAMPLE_INDEX)/(NUM_SAMPLES))
 #define FFT_FREQ_TO_SAMPLE(NUM_SAMPLES, FREQ) ((int)roundf((FREQ)*(NUM_SAMPLES)/44100))
 
-inline float FFT_PSD( complex c )
+inline float FFT_PSD( const complex& c )
 {
 #if STD_COMPLEX
 	return std::abs( c );
 #else
 	__m128 v = loadFloat2( &c );
-	v = _mm_dp_ps( v, v, 0b110001 );
+	// Funny enough, _mm_dp_ps appears to be slightly slower here, despite 1 instruction versus 3.
+	v = _mm_mul_ps( v, v );
+	v = _mm_add_ss( v, _mm_movehdup_ps( v ) );
 	v = _mm_sqrt_ss( v );
 	return _mm_cvtss_f32( v );
 #endif
